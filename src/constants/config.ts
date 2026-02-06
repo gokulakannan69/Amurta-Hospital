47cce1663e871626efe900e3b63b44f35d66f19d
gokulakannan s <gokulakannans624@mail.com>
2026-02-06 19:44:27 +0530

    Update project files

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,5 +1,5 @@
 export const AUTOMATION_CONFIG = {
-    scriptUrl: import.meta.env.VITE_GOOGLE_SCRIPT_URL || "https://script.google.com/macros/s/AKfycbx8m9CROkuOa0PAZ7vzT2TYawWmWwcNcH5PiC0LGRbqBrkVk4O5RP06Hklku6dpMo85_g/exec"
+    scriptUrl: import.meta.env.VITE_GOOGLE_SCRIPT_URL || "https://script.google.com/macros/s/AKfycbyzoSsUJitK2kinCwHmkQyzaxcZyt4CoNhEMblf5d_ny2PqrK_yQDCdqefJ0WlCwwfUEQ/exec"
 };
 
 export const DOCTOR_IMAGE_URL = "/doctor.jpg";
