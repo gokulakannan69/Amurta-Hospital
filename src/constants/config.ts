3bde8857a849673b80256b5eb871f08d744e1537
gokulakannan s <gokulakannans624@mail.com>
2026-02-06 19:56:47 +0530

    Update Google Script URL

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,5 +1,5 @@
 export const AUTOMATION_CONFIG = {
-    scriptUrl: import.meta.env.VITE_GOOGLE_SCRIPT_URL || "https://script.google.com/macros/s/AKfycbyzoSsUJitK2kinCwHmkQyzaxcZyt4CoNhEMblf5d_ny2PqrK_yQDCdqefJ0WlCwwfUEQ/exec"
+    scriptUrl: import.meta.env.VITE_GOOGLE_SCRIPT_URL || "https://script.google.com/macros/s/AKfycbxbDV5Y6sIG-FmI9kT-dYSlbi2MBY3ihTxcUZlaS1V30ZP4KbUdcjHpkgmAc0nU6VyDAw/exec"
 };
 
 export const DOCTOR_IMAGE_URL = "/doctor.jpg";
